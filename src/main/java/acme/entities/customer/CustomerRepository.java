bf85e79a9617ba6163fe5d11e79bf63d10bc7550
edubusluc <edubusluc@alum.us.es>
2025-03-12 19:48:18 +0100

    feat: Customer,Passenger & Booking sample data

---
+++ src/main/java/acme/entities/customer/CustomerRepository.java
@@ -0,0 +1,11 @@
+
+package acme.entities.customer;
+
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface CustomerRepository extends AbstractRepository {
+
+}
