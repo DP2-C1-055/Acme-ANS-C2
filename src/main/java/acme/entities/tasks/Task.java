35b856df3f67be12f2ec9a860cb86031b007eea3
JSalasM5 <jesussalasm99@gmail.com>
2025-03-13 11:21:38 +0100

    feat: Task entity (data according to missing-ranges.xlsx on date 05/03/25)

---
+++ src/main/java/acme/entities/tasks/Task.java
@@ -0,0 +1,42 @@
+
+package acme.entities.tasks;
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Task extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private TaskType			taskType;
+
+	@Mandatory
+	@ValidString(min = 1, max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 10)
+	@Automapped
+	protected Integer			priority;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 1000)
+	@Automapped
+	protected Integer			estimatedDuration;
+
+}
