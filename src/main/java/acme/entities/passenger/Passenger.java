d8ba099103a6c9e7182463c0283e6498a9a3a11a
edubusluc <edubusluc@alum.us.es>
2025-05-12 18:04:27 +0200

    feat: Add Booking and passenger indexes

--- src/main/java/acme/entities/passenger/Passenger.java
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
@@ -25,7 +27,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-//@ValidPassenger
+@Table(indexes = {
+	@Index(columnList = "draftmode")
+})
 public class Passenger extends AbstractEntity {
 
 	protected static final long	serialVersionUID	= 1L;
