5e8c98236b64338a9b7ddd42144b24222a11413a
Álvaro Carrera Bernal <alvcarber1@alum.us.es>
2025-07-03 16:13:39 +0200

    fix indexes

--- src/main/java/acme/realms/crew/Crew.java
+++ src/main/java/acme/realms/crew/Crew.java
@@ -3,7 +3,9 @@ package acme.realms.crew;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
@@ -24,6 +26,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidCrewIdentifier
+@Table(indexes = {
+	@Index(columnList = "availability")
+})
 public class Crew extends AbstractRole {
 
 	// Serialisation version --------------------------------------------------
